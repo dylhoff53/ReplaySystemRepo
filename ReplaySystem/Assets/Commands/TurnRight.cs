0ba319ceac8e467b3d4f387cdec743e837deb911
dhoffman548 <112724937+dylhoff53@users.noreply.github.com>
2022-09-16 12:25:32 -0700

    Repo
    woww

---
+++ ReplaySystem/Assets/Commands/TurnRight.cs
@@ -0,0 +1,15 @@
+namespace Chapter.Command
+{
+    public class TurnRight : Command
+    {
+        private BikeController _controller;
+        public TurnRight(BikeController controller)
+        {
+            _controller = controller;
+        }
+        public override void Execute()
+        {
+            _controller.Turn(BikeController.Direction.Right);
+        }
+    }
+}
